8f8a666f6f9eddfca43f2ce169b43a4711e87d70
Wanga-tracker <trackerwanga01@gmail.com>
2025-06-01 21:22:06 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,23 @@
 // firebase.js
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.8.2/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/11.8.2/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.8.2/firebase-firestore.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-storage.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjsxzqXwW2PcwowbFSb4jiGTHGv9rdL0w",
-  authDomain: "agent-hive-project.firebaseapp.com",
-  projectId: "agent-hive-project",
-  storageBucket: "agent-hive-project.firebasestorage.app",
-  messagingSenderId: "464476218600",
-  appId: "1:464476218600:web:59aa5ba90c73468d78155d",
-  measurementId: "G-YGQ85D592J"
+  apiKey: "AIzaSyCs6rzjsYBPYmT1vK3VWQRU36MJgQmKJq0",
+  authDomain: "edu-hub-kenya.firebaseapp.com",
+  projectId: "edu-hub-kenya",
+  storageBucket: "edu-hub-kenya.firebasestorage.app",
+  messagingSenderId: "163804145504",
+  appId: "1:163804145504:web:9b989b384a2bd2643f6005",
+  measurementId: "G-C8YB09R7J5"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
